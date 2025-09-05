0395f0be2087155f3a55d7ce3d6a35355b76b312
KEVIN <36017915+kevinguti@users.noreply.github.com>
2025-09-04 21:10:37 -0400

    test: primer test case

---
+++ test/title.spec.js
@@ -0,0 +1,23 @@
+const {test, expect} = require('@playwright/test');
+
+test('valida el titulo exacto', async ({ page }) => {
+  //abre la pagina example.com
+    await page.goto('https://example.com');
+
+  // Validar que el título de la página sea el esperado
+  await expect(page).toHaveTitle('Example Domain');
+});
+
+
+test ('valida titulo con regex (contiene palabra)', async ({ page }) => {
+  //abre la pagina example.com
+  await page.goto('https://example.com');
+
+  // Validar que el título de la página contenga la palabra "Example"
+  await expect(page).toHaveTitle(/Example/);
+});
+
+
+function sleep (time){
+  return new Promise((resolve) => setTimeout(resolve, time));
+}
